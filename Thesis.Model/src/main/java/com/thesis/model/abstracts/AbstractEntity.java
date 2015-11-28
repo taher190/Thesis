5b018281a8cbe5a999a29d768b4c1dddab9a8616
Tahir Arslan <taher190@hotmail.com>
2015-11-28 18:54:38 +0200

    Inıtial dev commit

---
+++ Thesis.Model/src/main/java/com/thesis/model/abstracts/AbstractEntity.java
@@ -0,0 +1,42 @@
+package com.thesis.model.abstracts;
+
+/**
+ * Tunahan - 2.11.2015.
+ */
+public abstract class AbstractEntity implements IEntity {
+
+    private Long id;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractEntity that = (AbstractEntity) o;
+
+        return !(id != null ? !id.equals(that.id) : that.id != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ Thesis.Model/src/main/java/com/thesis/model/abstracts/IEntity.java
@@ -0,0 +1,11 @@
+package com.thesis.model.abstracts;
+
+/**
+ * Tunahan - 2.11.2015.
+ */
+public interface IEntity {
+
+    Long getId();
+
+    void setId(Long id);
+}
