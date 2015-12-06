23b8ae6aa4008d5f24e2739810693e633b90cec0
taher190 <taher190@hotmail.com>
2015-12-06 14:45:38 +0200

    ..

--- Thesis.UI/src/main/java/com/thesis/controller/LoginBean.java
+++ Thesis.UI/src/main/java/com/thesis/controller/LoginBean.java
@@ -33,7 +33,7 @@ public class LoginBean {
     private IRoleService roleService;
 
     @PostConstruct
-    public void init() {/*
+    public void init() {/*//
         logger.info("call init method");
 
         Role role = new Role();
